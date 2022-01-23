58a6cb0cb6c47be6043182305b9638bccd2a74f4
Markus Klein <markus-klein@live.de>
2022-01-23 12:48:56 +0100

    introduce CompletionBody

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,35 @@
 use reqwest::{header, ClientBuilder};
+use serde::Serialize;
+
+/// The prompt for models can be a combination of different modalities (Text and Image). The type of
+/// modalities which are supported depend on the Model in question.
+#[derive(Serialize, Debug)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum Modality<'a> {
+    /// The only type of prompt which can be used with pure language models
+    Text { data: &'a str },
+}
+
+/// Body send to the Aleph Alpha API on the POST `/completion` Route
+#[derive(Serialize, Debug)]
+pub struct CompletionBody<'a> {
+    /// Name of the model tasked with completing the prompt. E.g. `luminus-base`.
+    pub model: &'a str,
+    /// Prompt to complete. The modalities supported depend on `model`.
+    pub prompt: &'a [Modality<'a>],
+    /// Limits the number of tokens, which are generated for the completion.
+    pub maximum_tokens: u32,
+}
 
 /// Sends HTTP request to the Aleph Alpha API
 pub struct Client {
-    pub base: String,
-    pub http: reqwest::Client,
+    base: String,
+    http: reqwest::Client,
 }
 
 impl Client {
-    /// In production you typically would want this to "https://api.aleph-alpha.de". Yet you may
-    /// want to use different instances for testing.
+    /// In production you typically would want set this to "https://api.aleph-alpha.de". Yet you may
+    /// want to use a different instances for testing.
     pub fn with_base_uri(base: String, token: &str) -> Self {
         let mut headers = header::HeaderMap::new();
 
@@ -21,15 +42,14 @@ impl Client {
             .default_headers(headers)
             .build()
             .unwrap();
-            
+
         Self { base, http }
     }
 
-    pub async fn complete(&self, task: String) -> String {
+    pub async fn complete(&self, task: &CompletionBody<'_>) -> String {
         self.http
             .post(format!("{}/complete", self.base))
-            .header("Content-Type", "application/json")
-            .body(task)
+            .json(task)
             .send()
             .await
             .unwrap()
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,11 +1,14 @@
-use aleph_alpha_client::Client;
+use aleph_alpha_client::{Client, CompletionBody, Modality};
 use wiremock::{
-    matchers::{body_string, header, method, path},
+    matchers::{body_json_string, header, method, path},
     Mock, MockServer, ResponseTemplate,
 };
 
 #[tokio::test]
 async fn completion_with_luminous_base() {
+
+    // Given
+
     // Start a background HTTP server on a random local part
     let mock_server = MockServer::start().await;
 
@@ -24,16 +27,23 @@ async fn completion_with_luminous_base() {
             format!("Bearer {}", token).as_str(),
         ))
         .and(header("Content-Type", "application/json"))
-        .and(body_string(body))
+        .and(body_json_string(body))
         .respond_with(ResponseTemplate::new(200).set_body_string(answer))
         // Mounting the mock on the mock server - it's now effective!
         .mount(&mock_server)
         .await;
 
+    // When
+    let task = CompletionBody {
+        model: "luminous-base",
+        prompt: &[Modality::Text { data: "Hello," }][..],
+        maximum_tokens: 1,
+    };
+
     let client = Client::with_base_uri(mock_server.uri(), token);
-    let response = client.complete(body.to_owned()).await;
+    let response = client.complete(&task).await;
 
+    // Then
     eprintln!("{}", response);
-
     assert_eq!(answer, response)
 }
